bd8f3f9d4d7c636c36e66ab4b75d54e41825038f
Gustavo <gustavopedrosateixeira@gmail.com>
2025-05-20 00:12:12 -0300

    ok

--- src/sevices/Main.java
+++ src/sevices/Main.java
@@ -3,10 +3,10 @@ package sevices;
 public class Main {
     public static void main(String[] args) {
 
-        Banco meuBanco = new Banco("Banco Exemplo", "123");
+        Banco meuBanco = new Banco("Banco F1", "123");
 
-        Cliente cliente1 = new Cliente("João Silva", "12345678901", "Rua A, 10");
-        Cliente cliente2 = new Cliente("Maria Oliveira", "98765432100", "Rua B, 20");
+        Cliente cliente1 = new Cliente("Ronny Raikkonen Oliveira", "07006377374", "Rua A, 10");
+        Cliente cliente2 = new Cliente("Veigar Hamilton da Silva", "98765432100", "Rua B, 20");
 
         ContaCorrente contaCorrente = new ContaCorrente("001", cliente1, 500.0);
         ContaPoupanca contaPoupanca = new ContaPoupanca("002", cliente2, 2.0);
@@ -23,7 +23,7 @@ public class Main {
         contaPoupanca.exibirInformacoes();
 
         System.out.println("\n=== Saque Conta Corrente ===");
-        contaCorrente.sacar(1200.0); // Saque com uso do limite especial
+        contaCorrente.sacar(1200.0);
         contaCorrente.exibirInformacoes();
 
         System.out.println("\n=== Saque Conta Poupança ===");
